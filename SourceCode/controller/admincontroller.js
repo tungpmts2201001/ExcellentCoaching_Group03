0e10961753faedd3d97f4b0a5b9eb292ce6762af
Loi <loinguyen300899@gmail.com>
2022-05-19 12:47:04 +0700

    ok

--- SourceCode/controller/admincontroller.js
+++ SourceCode/controller/admincontroller.js
@@ -9,3 +9,60 @@ function hidePopup() {
     modal.style.display = "none"
     window.open('admin.html')
 }
+
+var app = angular.module('myApp', []);
+app.controller('myCtrl', function($scope, $http){
+    //1. Đọc data từ fiel json
+    $http.get("DB.json")
+    .then(function(rspt) {
+        $scope.AdminList = rspt.data.Admins;
+    })
+
+//sort
+    $scope.sapxep = {};
+    $scope.doSapxep = function(cot){
+    var sapxep = $scope.sapxep;
+    if(sapxep.cot == cot){
+        sapxep.descending = !sapxep.descending;
+   
+    }
+    else{
+        sapxep.cot = cot;
+        sapxep.descending = false;
+
+       
+    }
+};
+
+//Record
+
+$scope.AdminModel ={}
+$scope.AdminList = [];
+
+$scope.Getrecord = function(Admin){
+    $scope.AdminModel.id = Admin.id;
+    $scope.AdminModel.UserName = Admin.UserName;
+    $scope.AdminModel.Password = Admin.Password;
+    $scope.btnUpdate = false;
+    $scope.btnSave = true;
+}
+
+ //3.Thêm data (c - create)
+ $scope.Add = function(){
+    var nation     = {
+        id: $scope.AdminModel.id,
+        UserName: $scope.AdminModel.UserName,
+        Password: $scope.AdminModel.Password
+    };
+
+    $scope.AdminList.push(nation);
+    setDefault();
+
+}
+
+  //7.Xóa record( D - deleted)
+  $scope.Delete = function(Admin) {
+    var _index = $scope.AdminList.indexOf(Admin);
+    $scope.AdminList.splice(_index, 1);
+    }
+});
